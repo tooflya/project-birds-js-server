fb9943f4caeec0ba672065c62881c740ef16959e
Igor Mats <igor.mats@yandex.ru>
2014-08-18 16:33:05 +0300

    huge update

--- app.js
+++ app.js
@@ -1,3 +1,111 @@
+var util = require("util");
+
+Array.prototype.last = function() {
+  return this[this.length - 1];
+};
+
+var online = {
+  users: {
+    count: 0,
+    list: []
+  },
+  battles: 0
+};
+var rooms = {
+  rooms: [],
+  create: function(socket) {
+    socket.server = true;
+
+    this.join({
+      users: [],
+      state: false
+    }, socket);
+  },
+  destroy: function(socket) {
+    for(var i = 0; i < 2; i++) {
+      var user = socket.room.users[i];
+
+      if(user) {
+        user.server = false;
+
+        user.emit('unsubscribe');
+      }
+    }
+
+    if(this.rooms.indexOf(socket.room) != -1) {
+      this.rooms.splice(this.rooms.indexOf(socket.room), 1);
+    }
+  },
+  join: function(room, socket) {
+    this.rooms.push(room);
+
+    room.users.push(socket);
+
+    socket.join(room);
+    socket.room = room;
+  },
+  leave: function(socket) {
+    if(socket.room.users.indexOf(socket) != -1) {
+      socket.room.users.splice(socket.room.users.indexOf(socket), 1);
+    }
+
+    socket.room = false;
+    socket.server = false;
+  },
+  add: function(socket, callback) {
+    if(callback) {
+      callback();
+    }
+
+    var entry = false;
+    for(var i = 0; i < this.rooms.length; i++) {
+      var room = this.rooms[i];
+
+      if(!room.state && room.users.length == 1) {
+        entry = room;
+
+        break;
+      }
+    }
+
+    if(entry) {
+      entry.state = true;
+      this.join(entry, socket);
+
+      setTimeout(function() {
+        for(var i = 0; i < 2; i++) {
+          var user = entry.users[i];
+
+          if(user) {
+            user.emit('subscribe', {
+              type: 'pending'
+            });
+          }
+        }
+      }, 2000);
+
+      setTimeout(function() {
+        for(var i = 0; i < 2; i++) {
+          var user = entry.users[i];
+
+          if(user) {
+            user.emit('subscribe', {
+              server: user.server,
+              type: 'start',
+              users: [
+                {name: entry.users[0].info.personal.name + " " + entry.users[0].info.personal.surname, photo: entry.users[0].info.personal.photo, weapon: entry.users[0].info.personal.weapon},
+                {name: entry.users[1].info.personal.name + " " + entry.users[1].info.personal.surname, photo: entry.users[1].info.personal.photo, weapon: entry.users[1].info.personal.weapon}
+              ]
+            });
+          }
+        }
+      }, 4000);
+    } else {
+      this.create(socket);
+    }
+  }
+};
+
 var mysql = require('mysql');
 
 var io = require('socket.io').listen(8082, {
@@ -8,10 +116,10 @@ io.sockets.on('connection', function(socket) {
   mysql.utilities = require('mysql-utilities');
 
   socket.connection = mysql.createConnection({
-    host: 'www.tooflya.com',
+    host: '127.0.0.1',
     user: 'root',
-    password: 'password',
-    database: 'www.project-birds.com'
+    password: 'RivaleCompany80632450512',
+    database: 'games.tooflya.com'
   });
 
   mysql.utilities.upgrade(socket.connection);
@@ -19,84 +127,92 @@ io.sockets.on('connection', function(socket) {
 
   socket.connection.connect();
 
-  socket.emit('message', 'Welcome to the our awesome server.');
+  socket.on('setup', function(data, callback) {
+    socket.info = {
+      personal: data
+    };
 
-  socket.on('disconnection', function(socket) {
-    socket.connection.end();
+    var users = [];
+    io.sockets.clients().forEach(function(user) {
+      if(user.info) {
+        users.push(user.info.personal);
+      }
+    });
+
+    io.sockets.emit('online', {
+      online: {
+        users: {
+          count: ++online.users.count,
+          add: users
+        },
+        battles: online.battles
+      }
+    });
+
+    callback();
   });
 
-  socket.on('authorize', function(data, callback) {
-    socket.identify = data.id;
+  socket.on('subscribe', function(data, callback) {
+    socket.info.personal.weapon = data.weapon;
+
     setTimeout(function() {
-      socket.connection.queryRow('SELECT * FROM `users` WHERE `identify` = ? LIMIT 1', [data.id], function(error, row) {
-        socket.emit('data', {
-          user: row
-        });
-
-        if(!row) {
-          socket.connection.insert('users', {
-            identify: data.id,
-            platform: data.platform
-          }, function(error, id) {
-          });
-        }
-      });
-    }, 1000);
+      rooms.add(socket, callback);
+    }, 2000);
   });
 
-  socket.on('leaderboard-id', function(data, callback) {
-    socket.connection.query('SELECT * FROM `users` ORDER by `score` DESC', function(error, rows, fields) {
-      var counter = 0;
-      rows.forEach(function(some) {
-        counter++;
+  socket.on('unsubscribe', function(data, callback) {
+    if(socket.room) {
+      rooms.destroy(socket);
+    }
 
-        if(socket.identify == some.identify) {
-          callback(counter, rows.length);
-
-          return;
-        }
-      });
-
-      callback(rows.length);
-    })
+    if(callback) {
+      callback();
+    }
   });
 
-  socket.on('leaderboard', function(data, callback) {
-    socket.connection.queryRow('SELECT * FROM `users` WHERE `identify` = ? LIMIT 1', [socket.identify], function(error, row) {
-      var player = row;
-      var counter = 0;
-      socket.connection.query('SELECT * FROM `users` ORDER by `score` DESC', function(error, rows, fields) {
-        var players = {};
+  socket.on('data', function(data, callback) {
+    if(socket.room) {
+      socket.broadcast.to(socket.room).emit('data', data);
 
-        players.id = [];
-        players.score = [];
+      if(callback) {
+        callback();
+      }
+    } else {
+      console.log('Error: user is trying to emit data to the own room but room is undefinied.');
+    }
+  });
 
-        rows.forEach(function(some) {
-          counter++;
+  socket.on('message', function(data, callback) {
+    if(socket.room) {
+      socket.broadcast.to(socket.room).emit('message', data);
 
-          players.id.push(some.identify);
-          players.score.push(some.score);
+      if(callback) {
+        callback();
+      }
+    } else {
+      console.log('Error: user is trying to emit data to the own room but room is undefinied.');
+    }
+  });
 
-          if(socket.identify == some.identify) {
-            player.place = counter;
+  socket.on('disconnect', function() {
+    if(socket.info) {
+      if(socket.info.personal) {
+        io.sockets.emit('online', {
+          online: {
+            users: {
+              count: --online.users.count,
+              remove: socket.info.personal
+            },
+            battles: online.battles
           }
         });
+      }
+    }
 
-        setTimeout(function() {
-          callback({
-            player: {
-              id: [player.identify],
-              score: player.score,
-              place: player.place
-            },
-            players: {
-              id: players.id,
-              score: players.score
-            },
-            users: counter
-          });
-        }, 1000);
-      });
-    });
+    if(socket.room) {
+      rooms.destroy(socket);
+    }
+
+    socket.connection.end();
   });
 });
